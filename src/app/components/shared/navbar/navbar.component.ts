38034a135e1c874de7f36f2ac3a4a846c478566f
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2017-11-19 18:16:40 +0100

    Aplicación Angular para hacer uso de Auth0, y logearse con una cuenta Google

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private authservice:AuthService) {
+        authservice.handleAuthentication();
+   }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.authservice.login();
+  }
+
+  logout(){
+    this.authservice.logout();
+  }
+
+}
